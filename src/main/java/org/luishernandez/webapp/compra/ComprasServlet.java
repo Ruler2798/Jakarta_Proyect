d616c530da553fe4d7d80174c9e69b0aaa47df27
Ruler2798 <lphg3940@gmail.com>
2024-05-14 14:54:32 -0600

    Primer Commit

---
+++ src/main/java/org/luishernandez/webapp/compra/ComprasServlet.java
@@ -0,0 +1,47 @@
+
+package org.luishernandez.webapp.compra;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author lphg3
+ */
+
+@WebServlet("/registro")
+public class ComprasServlet extends HttpServlet  {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.setContentType("text/html");
+       String username = req.getParameter("username");
+       String password = req.getParameter("password");
+       String email = req.getParameter("email");
+       try(PrintWriter out = resp.getWriter()){
+            out.println("</DOCTYPE html>");
+            out.println("<html>");
+            out.println("    <head>");
+            out.println("        <meta charset=\"UTF-8\" >");
+            out.println("        <title>Resultado Form</title>");
+            out.println("    </head>");
+            out.println("    <body>");
+            out.println("        <h1>Resultados de compra</h1>");
+            
+            out.println("           <ul>");
+            out.println("               <li> Username: " + username + "</li>");
+            out.println("               <li> Password: " + password + "</li>");
+            out.println("               <li> Email: " + email + "</li>");
+            out.println("           </ul>");
+            out.println("    </body>");
+            out.println("</hmtl>");
+       
+       }
+    }
+        
+}
