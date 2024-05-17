11fc2ee5bb9c531b6ce1f03450a711de19551851
Luis <lphg3940@gmail.com>
2024-05-17 11:31:32 -0600

    Agregando objetos

--- src/main/java/org/luishernandez/webapp/compra/ComprasServlet.java
+++ src/main/java/org/luishernandez/webapp/compra/ComprasServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -20,9 +22,23 @@ public class ComprasServlet extends HttpServlet  {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        resp.setContentType("text/html");
+       
        String username = req.getParameter("username");
        String password = req.getParameter("password");
        String email = req.getParameter("email");
+       
+       List <String> errores = new ArrayList<>();
+       
+       if (username == null || username.isBlank()){
+           errores.add("el username es requerido");
+       }
+       if (password == null || password.isBlank()){
+           errores.add("el passsword no puede ser vacio");
+       }
+       if(email == null || !email.contains("@") ){
+           errores.add("el email es requerido y debe tener un formato de correo");
+       }
+       
        try(PrintWriter out = resp.getWriter()){
             out.println("</DOCTYPE html>");
             out.println("<html>");
@@ -34,9 +50,17 @@ public class ComprasServlet extends HttpServlet  {
             out.println("        <h1>Resultados de compra</h1>");
             
             out.println("           <ul>");
+            if (errores.isEmpty()){
             out.println("               <li> Username: " + username + "</li>");
             out.println("               <li> Password: " + password + "</li>");
             out.println("               <li> Email: " + email + "</li>");
+            }else {
+                errores.forEach(error -> {
+                    out.println("<li>"+ error +"</li>");
+                   
+            });
+                out.println("<p><a href= \"/webApp-Compra/index.html\"");
+            }
             out.println("           </ul>");
             out.println("    </body>");
             out.println("</hmtl>");
